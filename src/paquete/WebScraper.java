b7382b7c646c662160e514e16925c6127573cf52
jesusgarciapereira <jesus.garciap@iesnervion.es>
2024-03-11 09:39:21 +0100

    Terminado con comentarios

--- src/paquete/WebScraper.java
+++ src/paquete/WebScraper.java
@@ -3,6 +3,9 @@ package paquete;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -12,7 +15,7 @@ public class WebScraper {
 	public static void main(String[] args) {
 		try {
 			// URL de la página web que queremos scrapear
-			String url = "https://cadenaser.com/radio-sevilla/";
+			String url = "https://www.antena3.com/";
 
 			// Realizar una solicitud HTTP para obtener el contenido de la página
 			Document doc = Jsoup.connect(url).get();
@@ -20,8 +23,10 @@ public class WebScraper {
 			// Seleccionar los elementos de interés utilizando selectores CSS o expresiones
 			// jQuery
 			Elements title = doc.select("title");
-			Elements titulos = doc.select("h2");
 			Elements enlaces = doc.select("a");
+			Elements clases = doc.select("*");
+			Elements forms = doc.select("form"); // Seleccionar formularios
+			Elements imagenes = doc.select("img");
 
 			// Crear un archivo HTML
 			BufferedWriter archivo = new BufferedWriter(new FileWriter("resultados.html"));
@@ -29,29 +34,73 @@ public class WebScraper {
 			archivo.write("<html>");
 			archivo.write("<head>");
 			archivo.write("<meta charset=\"UTF-8\">");
+			// Title extraido de la propia página
 			archivo.write("<title>Web Scraping de " + title.text() + "</title>");
 			archivo.write("</head>");
 			archivo.write("<body>");
-			archivo.write("<h1>" + title.text() + "</h1>");
-			// No sé si esta es la manera correcta
+			// Título
+			archivo.write("<h1>" + title.text() + "</a></h1>");
+
+			// Los 3 primeros enlaces encontrados
+			archivo.write("<h2>Enlaces</h2>");
 			archivo.write("<ul>");
-				archivo.write("<li><a href=\"https://cadenaser.com/podcast/radio-sevilla/la-camara-de-los-balones/517/\">La Cámara de los Balones</a></li>");
-				archivo.write("<li><a href=\"https://cadenaser.com/podcast/radio-sevilla/el-ambigu/1671/\">El Ambigú</a></li>");
-				archivo.write("<li><a href=\"https://cadenaser.com/podcast/radio-sevilla/cruz-de-guia/1666/\">Cruz de Guía</a></li>");
-			archivo.write("</ul>");
-			
-			archivo.write("<ul>");
+			int contadorEnlaces = 0;
 			for (Element enlace : enlaces) {
-			archivo.write("<li><a href=\"" + enlace.attr("href") + "\">" + enlace.text() + "</a></li>");
-		}
-			archivo.write("</ul>");
-			// Escribir los resultados en el archivo HTML
-			archivo.write("<ul>");
-			for (Element titulo : titulos) {
-				archivo.write("<li>" + titulo.text() + "</li>");
+				if (contadorEnlaces < 3) {
+					archivo.write("<li><a href=\"" + enlace.attr("href") + "\">" + enlace.text() + "</a></li>");
+					contadorEnlaces++;
+				} else {
+					break; // Salir del bucle después de mostrar 3 enlaces
+				}
 			}
 			archivo.write("</ul>");
 
+			// 3 primeras clases utilizadas
+			archivo.write("<h2>Clases</h2>");
+			archivo.write("<ul>");
+			int contadorClases = 0;
+			for (Element clase : clases) {
+				if (!clase.className().equals("") && contadorClases < 3) {
+					archivo.write("<li>" + clase.attr("class") + "</li>");
+					contadorClases++;
+				}
+			}
+			archivo.write("</ul>");
+
+			// Indicar si existe formulario y mostrar información
+			archivo.write("<h2>Formulario</h2>");
+			if (!forms.isEmpty()) {
+				archivo.write("<p>Sí</p>");
+				// Mostrar detalles del formulario
+				Element formulario = forms.first();
+				archivo.write("<p>Formulario redirige a: " + formulario.attr("action") + " con método "
+						+ formulario.attr("method") + "</p>");
+			} else {
+				archivo.write("<p>No</p>");
+			}
+			
+			
+			// Imágenes encontradas (mostrar las primeras 3 con un tamaño máximo de 120px de alto)
+			archivo.write("<h2>Imágenes</h2>");
+			archivo.write("<ul>");
+			
+			int imagenesMostradas = 0;
+
+			for (Element imagen : imagenes) {
+			    if (imagenesMostradas < 3) {
+			        String src = imagen.attr("src");
+			        String alt = imagen.attr("alt");
+
+			        // Agregar la etiqueta de imagen con un tamaño máximo de 120px de alto
+			        archivo.write("<li><img src=\"" + src + "\" alt=\"" + alt + "\" style=\"max-height: 120px;\"></li>");
+
+			        imagenesMostradas++;
+			    } else {
+			        break; // Salir del bucle después de mostrar 3 imágenes
+			    }
+			}
+
+			archivo.write("</ul>");
 			archivo.write("</body>");
 			archivo.write("</html>");
 
